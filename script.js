f23149ef064717bed2c2f4c7b64a70cad26e1f4e
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-05-20 22:29:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let modal = document.getElementById("mymodal");
+      let openBtn = document.getElementById("openModal");
+      let closeBtn = document.querySelector(".close-modal");
+      // open Modal
+      openBtn.onclick = function () {
+        modal.style.display = " block";
+      };
+      // close Modal when close btn clicked
+      closeBtn.onclick = function () {
+        modal.style.display = "none";
+      };
+      //    close modal on outside click
+      window.onclick = function () {
+        if (event.target === modal) {
+          modal.style.display = "none";
+        }
+      };
